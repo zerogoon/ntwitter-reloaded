08d2304dc5e9e87c504ea21d0c3aed585af6be71
zerogoon <kckzero@gmail.com>
2023-11-02 13:58:44 +0900

    firebase 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,67 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Layout from "./components/layout"
+import Home from "./routes/home"
+import Profile from "./routes/profile"
+import Login from "./routes/login"
+import CreateAccount from "./routes/create-account"
+import { createGlobalStyle } from "styled-components"
+import reset from "styled-reset"
+import { useEffect, useState } from "react"
+import LoadingScreen from "./components/loading-screen"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
+]);
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color: white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+    sans-serif;
+  }
+`;
+
 function App() {
+  const [isLoading, setLoading] = useState(true);
+  const init = async() => {
+    // wait for firebase
+    setLoading(false);
+  }
+  useEffect(() => {
+    init();
+  }, []);
   return (
-    <></>
+    <>
+      <GlobalStyles />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
+    </>
   )
 }
 
---
+++ src/firebase.tsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCP7V2KxVmoJ1-5P1kTxQiA3RGVOGheJ20",
+  authDomain: "ntwitter-reloaded.firebaseapp.com",
+  projectId: "ntwitter-reloaded",
+  storageBucket: "ntwitter-reloaded.appspot.com",
+  messagingSenderId: "916757456802",
+  appId: "1:916757456802:web:00fa3d70c0487c9d47b4fb"
+};
+
+const app = initializeApp(firebaseConfig);
