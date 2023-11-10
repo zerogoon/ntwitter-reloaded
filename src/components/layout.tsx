0fa501000a25457c3c542b64907c834546fb7c15
zerogoon <kckzero@gmail.com>
2023-11-10 15:44:43 +0900

    Protected Routes

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,9 @@
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
-    return (
-        <>
-            <h2>layout</h2>
-            <Outlet />
-        </>
-    );
+  return (
+    <>
+        <Outlet />
+    </>
+  );
 }
